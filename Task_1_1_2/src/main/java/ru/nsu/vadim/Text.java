257f5fbd21ec53ef79311cbf9489d879617d0ec7
Vadim Mostovoy <vadim@vadimMacBook.local>
2021-09-28 22:20:42 +0700

    Zfunction without tests

--- Task_1_1_2/src/main/java/ru/nsu/vadim/Text.java
+++ Task_1_1_2/src/main/java/ru/nsu/vadim/Text.java
@@ -1,36 +1,61 @@
 package ru.nsu.vadim;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.Reader;
+import java.io.StringReader;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.stream.Collectors;
+import java.util.List;
 
 public class Text {
-    public static void searchPattern(String pattern, String fileName) throws Exception {
-        int[] zArr;
-        int[] res = new int[1024];
-        int patternLen = pattern.length();
+    public static List<Integer> searchPatternFile(String pattern, String fileName) throws Exception {
         FileReader reader = new FileReader(fileName);
-        char[] buffer = new char[1024];
+        return searchPattern(pattern, reader);
+    }
+    public static List<Integer> searchPattern(String pattern, String string) throws Exception {
+        StringReader reader = new StringReader(string);
+        return searchPattern(pattern, reader);
+    }
+    public static List<Integer> searchPattern(String pattern, Reader reader) throws Exception {
+        int[] zArr;
+        ArrayList<Integer> res = new ArrayList<>(0);
+        int buffSize = 20;
+        int patternLen = pattern.length();
+        char[] buffer = new char[buffSize];
         for (int i = 0; i < patternLen; i++) {
             buffer[i] = pattern.charAt(i);
         }
+
         buffer[patternLen] = '$';
-        int eofCheck = 0;
-        int resCnt = 0;
+
+        int charRead;
         for (int i = 0; true; i++) {
-            eofCheck = reader.read(buffer, patternLen + 1, 1024 - patternLen - 1);
-            if (eofCheck == -1) {
+            if (i == 0) {
+                charRead = reader.read(buffer, patternLen + 1, buffSize - patternLen - 1);
+            } else {
+                charRead = reader.read(buffer, patternLen * 2, buffSize - patternLen * 2);
+            }
+            if (charRead == -1) {
                 break;
             }
-            zArr = calculateZ(buffer);
-            for (int j = patternLen + 1; j < 1024; j++) {
-                if (zArr[j] == patternLen) {
-                    System.out.println(j + i * 1024 - patternLen - 1);
+            if (charRead < buffSize - patternLen * 2) {
+                for (int c = patternLen * 2 + charRead; c < buffSize; c++){
+                    buffer[c] = '$';
                 }
             }
+            zArr = calculateZ(buffer);
+            for (int j = patternLen + 1; j < buffSize; j++) {
+                if (zArr[j] == patternLen) {
+//                    System.out.println(j + i * (buffSize - patternLen - 1) - patternLen - 1 - (patternLen - 1) * i);
+                    res.add(j + i * (buffSize - patternLen - 1) - patternLen - 1 - (patternLen - 1) * i);
+                }
+            }
+            if (charRead < buffSize - patternLen * 2) {
+                break;
+            }
+            System.arraycopy(buffer, buffSize - patternLen + 1, buffer, patternLen + 1, patternLen - 1);
         }
+        reader.close();
+        return res;
     }
 
     private static int[] calculateZ(char[] str) {
