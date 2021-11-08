de59f3f27cc9254f257c82b0a593118ef92fc45a
Vadim <vadimmos2002@gmail.com>
2021-11-08 21:54:15 +0700

    Fixes done (2)

--- Task_1_4_1/src/main/java/ru/nsu/vadim/console/OperationFabric.java
+++ Task_1_4_1/src/main/java/ru/nsu/vadim/console/OperationFabric.java
@@ -2,7 +2,22 @@ package ru.nsu.vadim.console;
 
 import ru.nsu.vadim.console.operations.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class OperationFabric {
+
+    protected static final List<String> UNARY_LIST = Arrays.asList("sin", "cos", "log", "sqrt");
+    protected static final List<String> BINARY_LIST = Arrays.asList("+", "-", "/", "*", "pow");
+
+    protected static boolean isUnaryOperationToken(String token) {
+        return UNARY_LIST.contains(token);
+    }
+
+    protected static boolean isBinaryOperationToken(String token) {
+        return BINARY_LIST.contains(token);
+    }
+
     public Operation createOperation(String operationToken, double a, double b) {
         return switch (operationToken) {
             case "+" -> new Add(a, b);
