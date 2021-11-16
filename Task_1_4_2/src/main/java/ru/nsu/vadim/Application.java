d3343525fea0cec9d8ffa14901359ab4e5812e05
Vadim <vadimmos2002@gmail.com>
2021-11-17 03:14:44 +0700

    Soft deadline check

---
+++ Task_1_4_2/src/main/java/ru/nsu/vadim/Application.java
@@ -0,0 +1,12 @@
+package ru.nsu.vadim;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Application.main");
+        LocalDateTime current = LocalDateTime.now();
+        System.out.println(current);
+    }
+}
---
+++ Task_1_4_2/src/main/java/ru/nsu/vadim/notebook/Note.java
@@ -0,0 +1,39 @@
+package ru.nsu.vadim.notebook;
+
+
+public class Note {
+//    из-за бага в gson тут именно String, а не LocalDateTime >:(
+    private String dateTime;
+    private String title;
+    private String text;
+
+    public Note(String dateTime, String title, String text) {
+        this.dateTime = dateTime;
+        this.title = title;
+        this.text = text;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
