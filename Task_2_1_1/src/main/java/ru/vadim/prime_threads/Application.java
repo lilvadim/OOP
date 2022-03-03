6524e7fdd550ec461dc41745e4056e4679dc1fa1
Vadim <32203382+lilvadim@users.noreply.github.com>
2022-03-03 21:55:54 +0700

    increased array size

--- Task_2_1_1/src/main/java/ru/vadim/prime_threads/Application.java
+++ Task_2_1_1/src/main/java/ru/vadim/prime_threads/Application.java
@@ -4,7 +4,7 @@ import static ru.vadim.prime_threads.utils.Benchmark.printResults;
 
 public class Application {
     public static void main(String[] args) {
-        Runner runner = new Runner(new TestInputGenerator(1000000).generateInputList());
+        Runner runner = new Runner(new TestInputGenerator(10000000).generateInputList());
 
         printResults(runner);
     }
