062bbd3c2d2ddbf4bdcb138b06aeb92455aa3a75
Vadim <32203382+lilvadim@users.noreply.github.com>
2022-03-27 21:02:34 +0700

    Не работает

--- Task_2_2_1/src/main/java/ru/nsu/vadim/Application.java
+++ Task_2_2_1/src/main/java/ru/nsu/vadim/Application.java
@@ -1,16 +1,25 @@
 package ru.nsu.vadim;
 
 import ru.nsu.vadim.collection.LimitedCapacityQueue;
+import ru.nsu.vadim.data.Order;
+import ru.nsu.vadim.data.OrderStatus;
 import ru.nsu.vadim.data.Pizza;
+import ru.nsu.vadim.employee.EmployeeManager;
+import ru.nsu.vadim.pizzeria.PizzaCooker;
+import ru.nsu.vadim.pizzeria.PizzaDeliverer;
+import ru.nsu.vadim.pizzeria.PizzeriaEmployeeManager;
 
-import java.time.Period;
+import java.io.File;
+import java.time.LocalDateTime;
 import java.util.ArrayDeque;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Queue;
-import java.util.concurrent.Callable;
+import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static java.lang.Math.abs;
 
 public class Application {
 
@@ -18,44 +27,60 @@ public class Application {
     private static final int M = 10;
     private static final int T = 5;
 
+    private static final Queue<Order<Pizza>> orders = new ArrayDeque<>();
+    private static final LimitedCapacityQueue<Order<Pizza>> storage = new LimitedCapacityQueue<>(T);
+    private static final AtomicBoolean active = new AtomicBoolean(false);
+
     public static void main(String[] args) {
-        Queue<Pizza> pizzaOrders = new ArrayDeque<>();
-        LimitedCapacityQueue<Pizza> storage = new LimitedCapacityQueue<>(T);
+        EmployeeManager employeeManager = new PizzeriaEmployeeManager();
+        inject(employeeManager);
+        ExecutorService executorService = Executors.newFixedThreadPool(N + M);
+        active.set(true);
+        employeeManager.getEmployees(PizzaCooker.class).forEach(executorService::submit);
+        employeeManager.getEmployees(PizzaDeliverer.class).forEach(executorService::submit);
+        generateOrders(10);
+        printOrders();
+    }
 
-        List<PizzaBaker> bakers = new ArrayList<>();
-        for (int i = 0; i < M; i++) {
-            bakers.add(
-                    new PizzaBaker(
-                            pizzaOrders,
-                            storage,
-                            1,
-                            Period.of(3, 4, 5))
-            );
-        }
-
-        List<PizzaDeliverer> deliverers = new ArrayList<>();
-        for (int i = 0; i < N; i++) {
-            deliverers.add(
-                    new PizzaDeliverer(
-                            storage, 4)
-            );
-        }
-
-        pizzaOrders.addAll(List.of(
-                new Pizza(600),
-                new Pizza(1000)
-        ));
-
-        ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+    private static void inject(EmployeeManager employeeManager) {
         try {
-            executor.invokeAll(
-                    bakers.stream().map(b -> (Callable<Boolean>) () -> {
-                        b.run();
-                        return true;
-                    }).toList()
-            );
-        } catch (InterruptedException e) {
+            employeeManager.addEmployeesFromJson(new File("employees.json"));
+        } catch (Exception e) {
             e.printStackTrace();
+        }
+        employeeManager.getEmployees(PizzaCooker.class).forEach(pizzaCooker -> {
+            pizzaCooker.setActive(active);
+            pizzaCooker.setStorage(storage);
+            pizzaCooker.setOrders(orders);
+        });
+        employeeManager.getEmployees(PizzaDeliverer.class).forEach(pizzaDeliverer -> {
+            pizzaDeliverer.setActive(active);
+            pizzaDeliverer.setStorage(storage);
+        });
+    }
+
+    private static void printOrders() {
+        System.out.println("+++++++++ ORDERS +++++++++");
+        orders.forEach(System.out::println);
+        System.out.println("++++++++++++++++++++++++++");
+        System.out.println();
+    }
+
+    private static void generateOrders(int orderCnt) {
+        Random random = new Random();
+        for (int i = 0; i < orderCnt; i++) {
+            orders.add(
+                    new Order<>(
+                            abs(random.nextLong()),
+                            List.of(
+                                    new Pizza(random.nextInt(1000)),
+                                    new Pizza(random.nextInt(1000))
+                            ),
+                            OrderStatus.IN_QUEUE,
+                            LocalDateTime.now(),
+                            random.nextLong(3)
+                    )
+            );
         }
     }
 }
--- Task_2_2_1/src/main/java/ru/nsu/vadim/pizzeria/PizzaDeliverer.java
+++ Task_2_2_1/src/main/java/ru/nsu/vadim/pizzeria/PizzaDeliverer.java
@@ -7,17 +7,35 @@ import ru.nsu.vadim.data.OrderStatus;
 import ru.nsu.vadim.data.Pizza;
 import ru.nsu.vadim.employee.AbstractEmployee;
 import ru.nsu.vadim.employee.Deliverer;
+import ru.nsu.vadim.employee.OrderSupplier;
 import ru.nsu.vadim.employee.WorkExperience;
 
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import static java.lang.Thread.sleep;
 
-public class PizzaDeliverer extends AbstractEmployee implements Deliverer<Pizza> {
+public class PizzaDeliverer extends AbstractEmployee implements Deliverer<Pizza>, Runnable {
 
     @JsonProperty("capacity")
     private final int capacity;
+    private OrderSupplier<Pizza> orderSupplier;
+    private AtomicBoolean active;
+    private final List<Order<Pizza>> baggage = new ArrayList<>();
+
+    public PizzaDeliverer(
+            long id,
+            WorkExperience workExperience,
+            int capacity,
+            Queue<Order<Pizza>> storage,
+            AtomicBoolean active) {
+        this(id, workExperience, capacity);
+        this.active = active;
+        orderSupplier = new OrderSupplier<>(storage);
+    }
 
     @JsonCreator
     public PizzaDeliverer(
@@ -28,16 +46,25 @@ public class PizzaDeliverer extends AbstractEmployee implements Deliverer<Pizza>
         this.capacity = capacity;
     }
 
+    public void setStorage(Queue<Order<Pizza>> storage) {
+        orderSupplier = new OrderSupplier<>(storage);
+    }
+
+    public void setActive(AtomicBoolean active) {
+        this.active = active;
+    }
+
     @Override
     public void deliver(Order<Pizza> order) {
         order.setStatus(OrderStatus.DELIVERING);
         System.out.println(this + " : " + order);
         try {
             sleep(TimeUnit.MILLISECONDS.toMillis(getWorkExperience().timeOfCompletingTask())
-                    * new Random().nextInt(5));
+                    * order.getLocationCoords());
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        baggage.remove(order);
         order.setStatus(OrderStatus.COMPLETE);
         System.out.println(this + " : " + order);
     }
@@ -45,5 +72,16 @@ public class PizzaDeliverer extends AbstractEmployee implements Deliverer<Pizza>
     @Override
     public int capacity() {
         return capacity;
+    }
+
+    @Override
+    public void run() {
+        while (active.get()) {
+            while (baggage.size() < capacity
+                    || (orderSupplier.isEmpty() && !baggage.isEmpty())) {
+                baggage.add(orderSupplier.get());
+            }
+            baggage.forEach(this::deliver);
+        }
     }
 }
