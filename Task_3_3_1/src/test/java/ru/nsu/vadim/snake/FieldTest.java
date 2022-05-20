c200856e3c533219af6ce3c331b9df270dca536e
Vadim <32203382+lilvadim@users.noreply.github.com>
2022-05-20 11:57:27 +0700

    FieldTest

---
+++ Task_3_3_1/src/test/java/ru/nsu/vadim/snake/FieldTest.java
@@ -0,0 +1,23 @@
+package ru.nsu.vadim.snake;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.nsu.vadim.snake.point.Point;
+
+public class FieldTest {
+    @Test
+    void generateFoodPoint() {
+        Field field = new Field(50, 50);
+        Point food = field.generateFoodPoint();
+
+        Assertions.assertEquals(food, field.get(food.x(), food.y()));
+    }
+
+    @Test
+    void generateObstacle() {
+        Field field = new Field(50, 50);
+        Point obstacle = field.generateObstaclePoint();
+
+        Assertions.assertEquals(obstacle, field.get(obstacle.x(), obstacle.y()));
+    }
+}
