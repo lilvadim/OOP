0c46d4f7760d51560a5f94f888b4871dece0845e
Vadim <32203382+lilvadim@users.noreply.github.com>
2022-06-05 20:55:50 +0700

    немного неверная структура была

--- Task_4/src/main/kotlin/ru/nsu/vadim/dsl/Tasks.kt
+++ Task_4/src/main/kotlin/ru/nsu/vadim/dsl/Tasks.kt
@@ -1,18 +1,10 @@
 package ru.nsu.vadim.dsl
 
 import java.time.LocalDate
-import java.time.format.DateTimeFormatter
 
 @ConfigMarker
-class Tasks(
-    private val tasks: MutableList<Task> = mutableListOf(),
-    val dateTimeFormatter: DateTimeFormatter
-) : MutableList<Task> by tasks {
-
+class Tasks(private val tasks: MutableList<Task> = mutableListOf()) : MutableList<Task> by tasks {
     fun task(id: String, deadline: LocalDate, init: Task.() -> Unit = {}) {
         tasks += Task(id, deadline).apply(init)
     }
-
-    fun task(id: String, deadline: String, init: Task.() -> Unit = {}) =
-        task(id, LocalDate.parse(deadline, dateTimeFormatter), init)
 }
