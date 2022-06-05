c0f9d3e613ef25b277f488e79db29a89b3765725
Vadim <32203382+lilvadim@users.noreply.github.com>
2022-06-05 19:46:27 +0700

    получение таска с соглашением имен Власова

--- Task_4/src/main/kotlin/ru/nsu/vadim/dsl/Task.kt
+++ Task_4/src/main/kotlin/ru/nsu/vadim/dsl/Task.kt
@@ -4,10 +4,15 @@ import java.time.LocalDate
 
 @ConfigMarker
 data class Task(var id: String, var deadline: LocalDate, var description: Description = Description()) {
+
+    fun toConventionName(): String = "Task_${convertId(id)}"
+
     data class Description(var id: String = "", var text: String = "", var score: Int = 1)
 
     fun description(id: String, init: Description.() -> Unit) {
         description = Description(id = id).apply(init)
     }
 }
+
+private fun convertId(id: String) = id.replace(".", "_")
 
